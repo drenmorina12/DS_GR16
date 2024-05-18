d339832095ecc201ac998d89c194427c0ab32917
Dren Morina <drenmorina321@gmail.com>
2024-05-19 00:43:07 +0200

    Create project structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,27 @@
 module com.example.ds_gr16 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
-    opens com.example.ds_gr16 to javafx.fxml;
-//    exports com.example.ds_gr16;
+
+    opens controller.Admin to javafx.fxml;
+    exports controller.Admin;
+//    opens controller.Overall to javafx.fxml;
+//    opens controller.User to javafx.fxml;
+
+
+
+//    opens app to javafx.fxml;
+    opens app to javafx.graphics;
+    exports app;
+
+    requires java.sql;
+//    requires jfreechart;
+//    requires jcommon;
+
+//    opens model to javafx.base;
+
+    exports model;
+
 }
