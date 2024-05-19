464b9c249eae1049451953986834d25cc12039c6
Djellza <jasiqidiellza@gmail.com>
2024-05-19 19:49:56 +0200

    add errorcontroller

---
+++ src/main/java/controller/Overall/Error404Controller.java
@@ -0,0 +1,28 @@
+package controller.Overall;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Error404Controller {
+    @FXML
+    private ImageView imgError404;
+
+
+    @FXML
+    public void initialize() {
+        try {
+            FileInputStream inputStream = new FileInputStream("Images/Error404.png");
+
+            Image image = new Image(inputStream);
+            imgError404.setImage(image);
+            // System.out.println("Image set successfully.");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Image not found");
+        }
+    }
+}
--- src/main/java/controller/Overall/ErrorController.java
+++
@@ -1,4 +0,0 @@
-package controller.Overall;
-
-public class ErrorController {
-}
